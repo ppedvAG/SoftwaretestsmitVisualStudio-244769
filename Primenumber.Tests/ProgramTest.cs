28adc4d2766c98c5ef3dbc1df140afab581c29d8
Ortwin Carstocea <OrtwinC@ppedv.de>
2025-01-20 10:59:15 +0100

    Lab 001 Primenumber Tests

---
+++ Primenumber.Tests/ProgramTest.cs
@@ -0,0 +1,53 @@
+﻿using Primenumbers;
+
+namespace PrimenumbersTests
+{
+    [TestClass]
+    public class ProgramTests
+    {
+        [TestMethod]
+        public void IsPrimenumber_InputIs2_ReturnsTrue()
+        {
+            // Arrange
+            int number = 2;
+
+            // Act
+            bool result = Program.IsPrimenumber(number);
+
+            // Assert
+            Assert.IsTrue(result);
+        }
+
+        [TestMethod]
+        [DataRow(2)]
+        [DataRow(3)]
+        [DataRow(5)]
+        [DataRow(7)]
+        [DataRow(11)]
+        [DataRow(13)]
+        [DataRow(17)]
+        [DataRow(19)]
+        [DataRow(23)]
+        [DataRow(97)]
+        public void IsPrimenumber_InputIsPrime_ReturnsTrue(int number)
+        {
+            Assert.IsTrue(Program.IsPrimenumber(number));
+        }
+
+        [TestMethod]
+        [DataRow(0)]
+        [DataRow(1)]
+        [DataRow(4)]
+        [DataRow(6)]
+        [DataRow(8)]
+        [DataRow(9)]
+        [DataRow(10)]
+        [DataRow(12)]
+        [DataRow(100)]
+        [DataRow(-7)]
+        public void IsPrimenumber_InputIsNotPrime_ReturnsFalse(int number)
+        {
+            Assert.IsFalse(Program.IsPrimenumber(number));
+        }
+    }
+}
---
+++ Primenumbers/Program.cs
@@ -0,0 +1,55 @@
+﻿namespace Primenumbers
+{
+    public class Program
+    {
+        internal static void Main(string[] args)
+        {
+            while (true)
+            {
+                Console.WriteLine("Bitte eine Priminput eingeben: ");
+                if (int.TryParse(Console.ReadLine(), out int input))
+                {
+                    if (IsPrimenumber(input))
+                    {
+                        Console.WriteLine($"Glückwunsch! {input} ist eine Primzahl.");
+                    }
+                    else
+                    {
+                        Console.WriteLine($"{input} ist leider keine Primzahl.");
+                    }
+                } 
+                else
+                {
+                    Console.WriteLine("Ungueltige Eingabe. Bitte erneut versuchen.");
+                }
+
+                Console.WriteLine("Weitere Primzahl pruefen? (j/N)");
+
+                if (Console.ReadLine().Equals("n", StringComparison.OrdinalIgnoreCase))
+                {
+                    break;
+                }
+            }
+        }
+
+        public static bool IsPrimenumber(int number)
+        {
+            if (number <= 1)
+                return false;
+
+            if (number == 2)
+                return true;
+
+            if (number % 2 == 0)
+                return false;
+
+            for (int i = 3; i < number; i += 2)
+            {
+                if (number % i == 0)
+                    return false;
+            }
+
+            return true;
+        }
+    }
+}
